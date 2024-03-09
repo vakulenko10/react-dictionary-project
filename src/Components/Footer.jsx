cd43d3e43779c23ec8caf6ee44fd351be36b825a
smmSiteAdmin <121180497+vakulenko10@users.noreply.github.com>
2024-03-09 23:17:28 +0100

    sadfsadf
    asdgasg

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,9 +11,9 @@ const Footer = () => {
             <div className='flex flex-col justify-center items-center md:flex-row py-5 px-3 md:px-10 md:justify-between  '>
             <ul className='links flex gap-1'>
                 <li><a href='https://www.instagram.com/vakulenko_10/' ><FaInstagram className={`transition ${theme == 'light'? 'fill-slate-700  hover:fill-slate-600':'fill-slate-300 hover:fill-slate-400 text-white'}`} size={30}  /></a></li>
-                <li><a href='https://www.linkedin.com/in/vitalik-vakulenko/'><FaLinkedin className={`transition ${theme == 'light'? 'fill-slate-700  hover:fill-slate-600':'fill-slate-300 hover:fill-slate-400 text-white'}`} size={30}  /></a></li>
+                {/* <li><a href='https://www.linkedin.com/in/vitalik-vakulenko/'><FaLinkedin className={`transition ${theme == 'light'? 'fill-slate-700  hover:fill-slate-600':'fill-slate-300 hover:fill-slate-400 text-white'}`} size={30}  /></a></li>
                 <li><a href='https://www.linkedin.com/in/vitalik-vakulenko/'><FaFacebookSquare  className={`transition ${theme == 'light'? 'fill-slate-700  hover:fill-slate-600':'fill-slate-300 hover:fill-slate-400 text-white'}`} size={30}  /></a></li>
-                
+                 */}
                 
             </ul>
             <div className='otherInfo flex gap-1'>
